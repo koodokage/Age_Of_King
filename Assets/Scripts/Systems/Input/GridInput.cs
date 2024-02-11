4fd49b80ca2ebe4b5e9e41b528c4f00aa1578ace
theblackaliens <koodokage@gmail.com>
2024-02-11 13:28:52 +0300

    added: scene structure, basic built in components

---
+++ Assets/Scripts/Systems/Input/GridInput.cs
@@ -0,0 +1,119 @@
+using UnityEngine;
+using AgeOfKing.System.Input;
+using UnityEngine.Assertions;
+
+namespace AgeOfKing.Component
+{
+    public class GridInput : MonoBehaviour
+    {
+        Camera _mainCamera;
+
+        [SerializeField] Grid grid;
+        [SerializeField] LayerMask gridInputLayer;
+        [SerializeField] LayerMask playerPawnInputLayer;
+        [SerializeField] GameObject symbol;
+        [SerializeField] GameObject selectedPawn;
+        [SerializeField] Vector3 targetLocation;
+        [SerializeField] bool movementToggle = false;
+        [SerializeField] bool isMoving = false;
+
+
+        private void Awake()
+        {
+            _mainCamera = Camera.main;
+
+            Assert.IsNotNull(_mainCamera);
+        }
+
+
+        private void OnEnable()
+        {
+            //Listen inputs
+            InputManager.OnMousePositionChanged += OnChange_MousePosition;
+            InputManager.OnMouseLeftClicked += OnClicked_MouseLeft;
+            InputManager.OnMouseRightClicked += OnClicked_MouseRight;
+        }
+
+        private void OnDisable()
+        {
+            //Release inputs
+            InputManager.OnMousePositionChanged -= OnChange_MousePosition;
+            InputManager.OnMouseLeftClicked -= OnClicked_MouseLeft;
+            InputManager.OnMouseRightClicked -= OnClicked_MouseRight;
+        }
+
+        /// <summary>
+        /// Move unit input trigger
+        /// </summary>
+        private void OnClicked_MouseRight()
+        {
+
+            if (selectedPawn == null)
+                return;
+
+            if (isMoving)
+                return;
+
+            movementToggle = true;
+            targetLocation = symbol.transform.position;
+        }
+
+        /// <summary>
+        /// Select unit input trigger
+        /// </summary>
+        void OnClicked_MouseLeft(Vector2 mouseLocation)
+        {
+            if (isMoving)
+                return;
+
+            Ray ray = _mainCamera.ScreenPointToRay(mouseLocation);
+            RaycastHit2D rayHit;
+
+            if (rayHit = Physics2D.Raycast(ray.origin, ray.direction, 100, playerPawnInputLayer))
+            {
+                selectedPawn = rayHit.transform.gameObject;
+            }
+        }
+
+        /// <summary>
+        /// Transform pointer data to world location
+        /// </summary>
+        /// <param name="mouseLocation"></param>
+        void OnChange_MousePosition(Vector2 mouseLocation)
+        {
+            Ray ray = _mainCamera.ScreenPointToRay(mouseLocation);
+            RaycastHit rayHit;
+            if (Physics.Raycast(ray, out rayHit, 100, gridInputLayer))
+            {
+                Vector3 gridLocation = grid.WorldToCell(rayHit.point);
+                symbol.transform.position = gridLocation;
+            }
+        }
+
+        private void Update()
+        {
+            //TEMPO CODE 
+
+            if (selectedPawn == null)
+                return;
+
+            if (movementToggle == false)
+                return;
+
+            if ((targetLocation - selectedPawn.transform.position).magnitude < 0.1f)
+            {
+                selectedPawn.transform.position = targetLocation;
+                movementToggle = false;
+                selectedPawn = null;
+                isMoving = false;
+                return;
+            }
+
+            selectedPawn.transform.position = Vector3.MoveTowards(selectedPawn.transform.position, targetLocation, Time.deltaTime * 4f);
+            isMoving = true;
+        }
+
+      
+    }
+
+}
---
+++ Assets/Scripts/Systems/Input/InputManager.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace AgeOfKing.System.Input
+{
+    public class InputManager : MonoBehaviour
+    {
+        private IA_Gameplay _gameplayInput;
+
+        //Entry point => mouse location changes
+        public delegate void PointerLocationChanged(Vector2 location);
+        public static PointerLocationChanged OnMousePositionChanged;
+        public delegate void PointerLeftClick(Vector2 location);
+        public static PointerLeftClick OnMouseLeftClicked;
+        public delegate void PointerRightClick();
+        public static PointerRightClick OnMouseRightClicked;
+
+        private void OnEnable()
+        {
+            //Create
+            _gameplayInput = new IA_Gameplay();
+            _gameplayInput.Enable();
+
+            //Subscribe
+            _gameplayInput.Pointer.Position.performed += MousePosition_performed;
+            _gameplayInput.Pointer.LeftClick.performed += LeftClick_performed;
+            _gameplayInput.Pointer.RightClick.performed += RightClick_performed;
+        }
+
+        private void RightClick_performed(InputAction.CallbackContext obj)
+        {
+            OnMouseRightClicked?.Invoke();
+        }
+
+        private void LeftClick_performed(InputAction.CallbackContext obj)
+        {
+            Vector2 mouseLocation = _gameplayInput.Pointer.Position.ReadValue<Vector2>();
+            OnMouseLeftClicked?.Invoke(mouseLocation);
+        }
+
+        private void MousePosition_performed(InputAction.CallbackContext obj)
+        {
+            Vector2 mouseLocation = obj.ReadValue<Vector2>();
+            OnMousePositionChanged?.Invoke(mouseLocation);
+        }
+
+        private void OnDisable()
+        {
+            //Unsubscribe
+            _gameplayInput.Pointer.Position.performed -= MousePosition_performed;
+            _gameplayInput.Pointer.LeftClick.performed -= LeftClick_performed;
+            _gameplayInput.Pointer.RightClick.performed -= RightClick_performed;
+        }
+
+    }
+
+}
